b60d08500fb40b4abcad56ca29f691d9e955f476
ldduc123 <ducld0510@gmail.com>
2024-05-11 17:25:28 +0700

    .

--- src/pages/HelpPage.jsx
+++ src/pages/HelpPage.jsx
@@ -1,11 +1,14 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 
 const HelpPage = () => {
   return (
     <div>
-      <NavLink to="name">Name</NavLink>
-      <NavLink to="age">Age</NavLink>
+      <div>
+        <NavLink to="name">Name</NavLink>
+        <NavLink to="age">Age</NavLink>
+      </div>
+      <Outlet />
     </div>
   );
 };
